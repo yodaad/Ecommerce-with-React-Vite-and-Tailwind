ec5bfe3035147cb0ef3c5d7a4157899dc3931a7d
yodaad@gmail.com <yodaad@gmail.com>
2023-09-01 17:25:44 -0500

    Basic Pages structure added

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-
-function App() {
-  return (
-    <div className="bg-sky-300">Ecommerce project ready to get started!</div>
-  );
-}
-
-export default App;
---
+++ src/Pages/MyOrder/index.jsx
@@ -0,0 +1,5 @@
+function Myorder() {
+  return <div className="bg-red-100">Myorder</div>;
+}
+
+export default Myorder;
