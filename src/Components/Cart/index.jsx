ee94df84e800da269acc4b8ffd3b70408167dadd
yodaad@gmail.com <yodaad@gmail.com>
2023-09-20 17:20:21 -0500

    Cart modal styles improved

--- src/Components/Cart/index.jsx
+++ src/Components/Cart/index.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const Cart = ({ closeModal }) => {
   return (
-    <div className="flex flex-col relative bg-zinc-50 border-2 border-blue-500 rounded-lg w-[1000px]">
+    <div className="flex flex-col relative bg-zinc-50 border-2 border-blue-500 rounded-lg w-[1000px] h-[800px]">
       <button
         className="absolute top-0 right-0 p-3 border-none bg-transparent text-zinc-500 cursor-pointer"
         onClick={() => closeModal(false)}
