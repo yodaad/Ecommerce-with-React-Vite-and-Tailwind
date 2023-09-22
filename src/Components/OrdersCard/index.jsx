89a7e404ef1ca78496bcf0fa6be5b0518d8976ef
yodaad@gmail.com <yodaad@gmail.com>
2023-09-22 15:47:21 -0500

    OrdersCard component added

---
+++ src/Components/OrdersCard/index.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+const OrdersCard = (props) => {
+  const { totalPrice, totalProducts } = props;
+
+  return (
+    <div className="flex justify-between items-center my-3 border border-black">
+      <p>
+        <span>01.02.203</span>
+        <span>{totalProducts}</span>
+        <span>{totalPrice}</span>
+      </p>
+    </div>
+  );
+};
+
+OrdersCard.propTypes = {
+  totalPrice: PropTypes.number.isRequired,
+  totalProducts: PropTypes.number.isRequired,
+};
+
+export { OrdersCard };
