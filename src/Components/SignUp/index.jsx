813191dab1b64aa4b3e5eb3c967fc88f4bc06486
yodaad@gmail.com <yodaad@gmail.com>
2023-09-29 15:36:52 -0500

    SignUp component added and styles applied, MyAccount page styles applied

---
+++ src/Components/SignUp/index.jsx
@@ -0,0 +1,35 @@
+const SignUp = () => {
+  return (
+    <div className="flex flex-col relative bg-zinc-50 border-2 border-blue-500 rounded-lg w-[500px] h-[380px] mt-10">
+      <h2 className="flex justify-center items-center text-2xl font-bold my-4">
+        Welcome
+      </h2>
+      <div className="my-4 mx-6">
+        <p>Your name: </p>
+        <input
+          type="text"
+          placeholder="Peter"
+          className="rounded-lg border border-blue-500 w-full my-2 p-1"
+        />
+        <p>Your email: </p>
+        <input
+          type="text"
+          placeholder="peter@peterland.com"
+          className="rounded-lg border border-blue-500 w-full my-2 p-1"
+        />
+        <p>Your password: </p>
+        <input
+          type="text"
+          className="rounded-lg border border-blue-500 w-full my-2 p-1"
+        />
+      </div>
+      <div className="flex justify-center">
+        <button className="flex justify-center items-center bg-blue-500 rounded-lg text-white text-lg w-3/4 h-10 my-2 p-4 cursor-pointer">
+          Create Account
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export { SignUp };
--- src/Pages/MyAccount/index.jsx
+++ src/Pages/MyAccount/index.jsx
@@ -1,7 +1,24 @@
 import { Layout } from "../../Components/Layout";
 
 function MyAccount() {
-  return <Layout>MyAccount</Layout>;
+  return (
+    <Layout>
+      <div className="flex flex-col relative bg-zinc-50 border-2 border-blue-500 rounded-lg w-[500px] h-[240px] mt-10">
+        <h2 className="flex justify-center items-center text-2xl font-bold my-4">
+          My Account
+        </h2>
+        <div className="my-4 ml-6">
+          <p>Name: </p>
+          <p>Email: </p>
+        </div>
+        <div className="flex justify-center">
+          <button className="flex justify-center items-center bg-blue-500 rounded-lg text-white text-lg w-3/4 h-10 mt-4 mb-2 p-4 cursor-pointer">
+            Edit
+          </button>
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 export { MyAccount };
